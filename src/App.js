34a8ead13a0aeaa063812f7e9341539c7dc7a5d0
Naramsim <igougi.ui@gmail.com>
2026-07-05 10:15:27 +0200

    feat: remove gql deprecation banner/remove gads

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
     return (
         <Root>
             <Head>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9300109033485921" crossorigin="anonymous"></script>                
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-X3X3FV0WED"></script>
                 <script>
                 {
